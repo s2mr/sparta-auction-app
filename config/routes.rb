91105fa103737453b57df66e03d4bafbde1bc322
kazu <g031n085@s.iwate-pu.ac.jp>
2017-07-22 01:24:02 +0000

    create view for new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'items/:id' => 'items#show'
-  get 'items' => 'items#index'
+  get '/items' => 'items#index'
+  post 'items' => 'items#create' #まずビューが呼ばれる訳ではなく、コントローラのメソッドが呼ばれる
+  get 'items/new' => 'items#new'
+  get '/items/:id' => 'items#show'
 end
