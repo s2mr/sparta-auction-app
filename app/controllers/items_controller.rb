5eb095d8c0bf8237f6884655218020205f044cbe
kazu <g031n085@s.iwate-pu.ac.jp>
2017-07-22 02:15:19 +0000

    create view for edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,5 +13,30 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to "/items/#{@item.id}"
+  end
+  
+  private
+  
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+  
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/items' => 'items#index'
-  post 'items' => 'items#create' #まずビューが呼ばれる訳ではなく、コントローラのメソッドが呼ばれる
-  get 'items/new' => 'items#new'
+  get '/items/new' => 'items#new'
+  post '/items' => 'items#create' #まずビューが呼ばれる訳ではなく、コントローラのメソッドが呼ばれる
   get '/items/:id' => 'items#show'
+  get '/items/:id/edit' => 'items#edit'
+  patch '/items/:id' => 'items#update'
 end
